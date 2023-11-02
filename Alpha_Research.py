2a06fe3723cdd9c099911f9350b09f43ba233b96
Ji Seob Lim <67276566+jimmylim0823@users.noreply.github.com>
2023-11-03 03:13:10 +0900

    Add files via upload

---
+++ Alpha_Research.py
@@ -0,0 +1,168 @@
+import os
+import pandas as pd
+import numpy as np
+import joblib
+import matplotlib.pyplot as plt
+
+class Alpha_Research():
+    def __init__(self, data_path="data", file_name="data.sav"):
+        file_path = os.path.join(data_path, file_name)
+        datas = joblib.load(file_path)
+        self.data_daily = datas[0]
+        self.data_monthly_acc = datas[1]
+        self.data_monthly_mkt = datas[2]
+        self.data_index = datas[3]
+        self.data_classification = datas[4]
+        self.data_weight = datas[5]
+        
+        # only for use of formulaic alpha
+        self.d = datas[0]
+        self.m = {**datas[1], **datas[2]}
+
+        # delay-1 alpha
+        self.df_target = (self.data_daily['ADJO'] + self.data_daily['ADJH']).copy()
+        self.df_target += (self.data_daily['ADJL'] + self.data_daily['ADJC']).copy()
+        self.df_target = self.df_target / 4
+        self.df_target = self.df_target.resample('M').first().shift(-1).iloc[:-1,:]
+        self.df_target = self.df_target[self.df_target.index>list(self.data_weight.keys())[0]]
+        self.df_target.index = list(self.data_weight.keys())[:-1]
+        self.df_target = (1 + self.df_target.pct_change()).apply(np.log)
+        self.df_target = self.df_target.shift(-1).iloc[:-1,:]
+
+        self.fgsc = self.data_classification['FGSC'].fillna(method='bfill').iloc[0]
+        self.sector_dict = dict()
+        for dt in self.df_target.index.to_list():
+            temp_dict = dict()
+            for sc in self.fgsc.unique():
+                sc_temp = self.fgsc[self.fgsc==sc].index.to_list()
+                temp_dict[sc] = [i for i in self.data_weight[dt].keys() if i in sc_temp]
+            self.sector_dict[dt] = temp_dict
+    
+    def data_help(self):
+        print("Daily: ", list(self.data_daily.keys()))
+        print("Monthly Accounting: ", list(self.data_monthly_acc.keys()))
+        print("Monthly Market: ", list(self.data_monthly_mkt.keys()))
+    
+    def neutralize(self, x):
+        demean = x - x.mean()
+        return demean / abs(demean).sum()
+    
+    
+    def sharpe_ratio(self, x):
+        return x.mean() / x.std() * np.sqrt(12)
+
+
+    def max_drawdown(self, x):
+        cum_ret = (1 + x).cumprod()
+        mdd = cum_ret / cum_ret.cummax() - 1
+        return mdd.min()
+
+
+    def ls_count(self, x):
+        stats = {}
+        stats['Min Long Count'] = x[x>0.000001].count(axis=1).resample('Y').min()
+        stats['Min Short Count'] = x[x<-0.000001].count(axis=1).resample('Y').min()
+        df = pd.DataFrame(stats)
+        years = list(range(df.index[0].year, df.index[-1].year+1))
+        df.index = years
+        return df.T
+
+        
+    def backtest(self, features, operation=None, chart=False, trunc=0.05):
+        if type(features) == pd.core.frame.DataFrame:
+            features = [features]
+        else:
+            pass
+        
+        backtest_dict = dict()
+        weight_dict = dict()
+        for dt in list(self.sector_dict.keys()):
+            temp_cross = [i.loc[dt] for i in features]
+            backtest_dict[dt] = 0.0
+            temp = []
+            sectors = self.sector_dict[dt]
+            for sc in list(sectors.keys()):
+                temp_sector = [i[sectors[sc]] for i in temp_cross]
+                if operation == None:
+                    temp_scores = temp_sector[0]
+                else:
+                    temp_scores = operation(temp_sector)
+                temp_target = self.df_target[sectors[sc]].loc[dt]
+                temp_weight = self.neutralize(temp_scores) / len(sectors.keys())
+                temp_weight = temp_weight.clip(lower=-trunc, upper=trunc)
+                temp_weight = temp_weight.apply(lambda x: 0.0 if -1e-6 < x < 1e-6 else x)
+                temp_result = temp_weight * temp_target
+                backtest_dict[dt] += temp_result.sum()
+                temp.append(temp_weight)
+            weight_dict[dt] = temp
+
+        bt_series = pd.Series(backtest_dict)
+        weights = [pd.concat(weight_dict[i]) for i in list(weight_dict.keys())]
+        df_weights = pd.concat(weights, axis=1).T
+        
+        if chart == True:
+            fig, ax = plt.subplots()
+            (1 + bt_series).cumprod().plot(ax=ax)
+            sr = self.sharpe_ratio(bt_series)
+            mdd = self.max_drawdown(bt_series)
+            ax.set_title(f"Sharpe Ratio: {sr:.2f} Max-Drawdown: {mdd:.2f}")
+            ax.set_xlabel("Date")
+            ax.set_ylabel("Portfolio Value")
+            
+            print(self.ls_count(df_weights))
+            
+        else:
+            pass
+        return bt_series, df_weights
+
+
+    def generate_weight(self, features, operation=None, trunc=0.05):
+        if type(features) == pd.core.frame.DataFrame:
+            features = [features]
+        else:
+            pass
+        
+        weight_dict = dict()
+        for dt in list(self.sector_dict.keys()):
+            temp_cross = [i.loc[dt] for i in features]
+            temp = []
+            sectors = self.sector_dict[dt]
+            for sc in list(sectors.keys()):
+                temp_sector = [i[sectors[sc]] for i in temp_cross]
+                if operation == None:
+                    temp_scores = temp_sector[0]
+                else:
+                    temp_scores = operation(temp_sector)
+                temp_weight = self.neutralize(temp_scores) / len(sectors.keys())
+                temp_weight = temp_weight.clip(lower=-trunc, upper=trunc)
+                temp_weight = temp_weight.apply(lambda x: 0.0 if -1e-6 < x < 1e-6 else x)
+                temp.append(temp_weight)
+            weight_dict[dt] = temp
+
+        weights = [pd.concat(weight_dict[i]) for i in list(weight_dict.keys())]
+        df_weights = pd.concat(weights, axis=1).T
+        
+        return df_weights
+    
+    def alpha_pool(self, alphas, topn=5):
+        rets = [alphas[i][0] for i in alphas.keys()]
+        ws = [alphas[i][1] for i in alphas.keys()]
+
+        # find top correlation pairs
+        df_corr = pd.DataFrame(rets, index=alphas.keys()).T.corr()
+        correlation_list = []
+        for i in range(len(df_corr.columns)):
+            for j in range(i+1, len(df_corr.columns)):
+                pair = (df_corr.columns[i], df_corr.columns[j], df_corr.iloc[i, j])
+                correlation_list.append(pair)
+        correlation_list.sort(key=lambda x: abs(x[2]), reverse=True)
+        i = 0
+        for pair in correlation_list:
+            if i >= topn:
+                break
+            print(f"{pair[0]} and {pair[1]} have correlation: {pair[2]:.2f}")
+            i += 1
+
+        # backtest equal weighted portfolio
+        df_ew = pd.concat(ws, axis=1).groupby(level=0, axis=1).mean()
+        _ = self.backtest(df_ew, chart=True)
